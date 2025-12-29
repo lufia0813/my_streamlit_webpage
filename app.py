3e0f46f09643825f790a0d3d9386681e871e6ff3
Baron <54239677+lufia0813@users.noreply.github.com>
2025-12-29 21:06:29 +0800

    Update app.py
    加入網站icon

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+st.set_page_config(
+    page_title="絲瓜的第一個網站",    
+    page_icon="🐸",               
+    layout="centered"            
+)
 st.title("歡迎來到絲瓜的AI練習小網頁")
 
 
