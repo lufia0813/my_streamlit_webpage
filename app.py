50fba89d27e18bf150714a9409bb422d8b5d2958
Baron <54239677+lufia0813@users.noreply.github.com>
2025-12-29 16:57:30 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("歡迎來到絲瓜的AI練習小網頁")
+
+
+st.write("睡覺時間到了～")
+st.image("capoo.png")
---
+++ pages/1_🎈_ 慶祝一下.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("🎈 慶祝一下")
+st.write("沒什麼特別的事，就是想慶祝！")
+if st.button("點我！點我！"):
+    st.balloons()
+    st.write("耶！！嗚呼🎉")
