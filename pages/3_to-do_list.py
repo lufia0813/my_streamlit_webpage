394517a47d5912e6768c4c72587e8b942c649b92
Baron <54239677+lufia0813@users.noreply.github.com>
2025-12-30 21:11:19 +0800

    Update 3_to-do_list.py

--- pages/3_to-do_list.py
+++ pages/3_to-do_list.py
@@ -63,14 +63,14 @@ for index, task in enumerate(st.session_state.tasks):
 			unsafe_allow_html = True
 		)
 
-	'''
+"""
 		background-color:{bg_color}; # 會根據是奇數還是偶數來填入背景顏色
 		padding: 15px; # 內距
 		border-radius: 10px; # 圓角
 		margin-bottom: 10px; # 在框的下面，空出 10 px 的距離。
 		📌 <b>{index + 1}.</b> {task} # <b> ... </b>是粗體
 		unsafe_allow_html = True # 允許在python中寫一點html
-	'''
+"""
 		
 	with c2:
 		st.write("")
